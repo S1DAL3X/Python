650c4582e5010e99e9ce02ed2e63fc3bbacd0aa2
Aleksey <sidorenkoalekse@mail.ru>
2018-01-25 13:38:56 +0700

    Update hero.py

--- python_game/hero.py
+++ python_game/hero.py
@@ -23,3 +23,24 @@ class Hero():
     def moving(self):
         ''' Start moving '''
         print(self.name + ' is moving ...')
+        
+        
+class SuperHero(Hero):
+    ''' Make our super hero. He is can use magic! '''
+    def __init__(self, name, level, race, health, magiclevel):
+        ''' Initiate our super hero '''
+        super().__init__(name, level, race, health)
+        self.magiclevel = magiclevel
+        self.magic = 100
+        
+    def makemagic(self):
+        ''' Hero use magic '''
+        self.magic -= 10
+        
+    def level_magic(self):
+        ''' Up our magic level '''
+        self.magiclevel += 1
+    
+    def show_hero(self):
+        message = 'Name : {}'.format(self.name) + '\nLevel : {}'.format(str(self.level)) + '\nRace : {}'.format(self.race) + '\nHealth : {}'.format(str(self.health)) + '\nMagic : {}'.format(str(self.magic) + '\nMagic level : {}'.format(str(self.magiclevel))) 
+        print(message + '\n')
