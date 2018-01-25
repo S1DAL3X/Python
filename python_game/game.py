64f3431f6cd979f2b0d6722385e8d4e217d2495f
Aleksey <sidorenkoalekse@mail.ru>
2018-01-25 13:38:15 +0700

    Update game.py

--- python_game/game.py
+++ python_game/game.py
@@ -2,10 +2,21 @@ from hero import *
 
         
 hero1 = Hero('S1DAL3X', 1, 'Human', 100)
-hero1.show_hero()
 hero1.set_health(150)
 hero1.level_up()
 hero1.show_hero()
-hero1.moving()
+
+hero2 = SuperHero('GodFather', 5, 'Magician', 100, 10)
+hero2.show_hero()
+hero2.level_up()
+hero2.level_magic()
+
+hero2.makemagic()
+hero2.makemagic()
+hero2.makemagic()
+
+hero2.show_hero()
+hero2.set_health(200)
+hero2.show_hero()
 
         
