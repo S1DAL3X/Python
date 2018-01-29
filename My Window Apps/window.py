5810598669d2026c829e6d065d6c4b42b0623a3f
Aleksey <sidorenkoalekse@mail.ru>
2018-01-29 16:41:45 +0700

    Add files via upload

---
+++ My Window Apps/window.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+root = Tk()
+root.title('Window')
+
+maxSizeWindow = root.maxsize()
+
+weight = (maxSizeWindow[0] // 2) - 1920 // 2 - 9
+height = (maxSizeWindow[1] // 2) - 1080 // 2
+
+root.geometry('1920x1080+' + str(weight) + '+' + str(height))
+
+def button_click():
+    print('Hello World!')
+
+def close_window():
+    root.destroy()
+    root.quit()
+
+
+#button = Button(root, text = 'Press Button', width = 10, height = 5, command = button_click )
+#button.place(x = 300, y = 100)
+
+root.protocol('WM_DELETE_WINDOW', close_window)
+
+root.mainloop()
