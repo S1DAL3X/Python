cb10ef64812b087f4704eca719794b432a6099f5
Aleksey <sidorenkoalekse@mail.ru>
2020-04-07 21:24:57 +0700

    Update SFUPageUpdater.py

--- SFUPageUpdater.py
+++ SFUPageUpdater.py
@@ -1,40 +1,59 @@
 #!/usr/bin/python3
 #Script to update html page on sfu sites
-#Default updating one time in 3 min
-#По умолчанию - скрипт посылает POST запрос на сайт один раз в 3 минуты
+#Default updating one time in (1 - 3) min
+#По умолчанию - скрипт посылает POST запрос на сайт один раз в (1 - 4) минуты
+import sys
+import random
+import colorama
+import requests
+import datetime, time
+from colorama import Fore, Back, Style
+from bs4 import BeautifulSoup
 
-import requests									#библиотека для обработки http запросов
-import datetime, time								#библиотеки для работы с системным временем
+LOGIN    = str(input('\nEnter SFU-login: '))
+PASSWORD = str(input('Enter SFU-password: '))
+AUTH_URL = 'https://i.sfu-kras.ru'
 
-LOGIN	 = str(input('\nEnter SFU-login: '))					#вводим СФУ логин
-PASSWORD = str(input('Enter SFU-password: '))					#вводим СФУ пароль
-AUTH_URL = 'https://i.sfu-kras.ru'						#ссылка на сайт
-
-#данные (http-заголовки), которые будем посылать сайту
 DATA = {'AUTH_FORM':'Y',
-	'TYPE':'AUTH',
-	'backurl':'/index.php',
-	'USER_LOGIN':LOGIN,
-	'USER_PASSWORD':PASSWORD}
+        'TYPE':'AUTH',
+        'backurl':'/index.php',
+        'USER_LOGIN':LOGIN,
+        'USER_PASSWORD':PASSWORD}
 
-#создали объект сессии (чтобы каждый раз отправлять на сайт одни и те же данные)
 session = requests.Session()
 
-#функция обработки запросов
+
+def get_online_status():
+        html_page       = session.post(AUTH_URL, DATA).text
+        soup_object     = BeautifulSoup(html_page, 'lxml')
+        user_online     = soup_object.find('span', id = 'user-name').text
+        return str(user_online)
+
+
 def post_request():
-	while True:
-		SYSTEM_TIME  = datetime.datetime.now()				#системная дата
-		CURRENT_TIME = SYSTEM_TIME.strftime('%X')			#системное время в читабельном формате
-		if session.post(AUTH_URL, DATA):				#если запрос отправлен, то:
-			print(str(CURRENT_TIME + ' - REQUEST IS SENT!'))	#вывести время + REQUEST IS SENT!
-		else:								#иначе, 		
-			print('ERRORL REQUEST IS NOT SENT!')			#вывести ERRORL REQUEST IS NOT SENT!
-		time.sleep(180)							#цикл засыпает на три минусы(180 сек) и работает заново
-
-#если запрос на сайт вернул код 200 (запрос успешно обработан), то запускаем функцию
+        try:
+                print(Fore.GREEN + get_online_status() + ' - Online\n')
+        except:
+                print(Fore.RED + 'WRONG LOGIN OR PASSWORD! PLEASE CHECK IT.\n')
+                sys.exit()
+        while True:
+                SYSTEM_TIME  = datetime.datetime.now()                                                                                                                                      
+                CURRENT_TIME = SYSTEM_TIME.strftime('%X')                                                                                                                                   
+                if session.post(AUTH_URL, DATA):                                                                                                                                            
+                        print(Fore.GREEN + str(CURRENT_TIME + ' - REQUEST IS SENT!'))                                                                                                       
+                else:                                                                                                                                                                       
+                        print(Fore.RED + 'ERRORL REQUEST IS NOT SENT!')                                                                                                                     
+                time_range = random.randint(60, 240)                                                                                                                                        
+                time.sleep(time_range)                                                                                                                                                      
+                                                                                                                                                                                            
+                                                                                                                                                                                            
 if requests.get(AUTH_URL).status_code == 200:
-	print('URL is true! Connecting... \n')
-	post_request()
+        print(Fore.CYAN + 'URL is online! Connecting... \n')
+        if get_online_status != False:
+                post_request()
+        else:
+                print('Error!')
 else:
-	print('Ups...error((')
-
+        print(Fore.RED + 'Ups...error ' + str(requests.get(AUTH_URL).status_code))
+	
+print(Style.RESET_ALL)
