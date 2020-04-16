d1b46edb6431f03a146b6ff08f6c46f8ab563138
Aleksey <sidorenkoalekse@mail.ru>
2020-04-16 19:21:30 +0700

    Add files via upload

--- py_socket/PyChat.py
+++ py_socket/PyChat.py
@@ -3,7 +3,7 @@ from threading import Thread
 
 def server_side():
     SERVER_ADDR = '0.0.0.0'
-    SERVER_PORT = int(input('SERVER_PORT = '))
+    SERVER_PORT = int(input('SERVER_SIDE_PORT = '))
     
     sock_serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock_serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -28,14 +28,14 @@ def client_side():
     print('\n')
     
     while True:
-        message = input('Message: ')
+        message = input('')
         message = str(USER_NAME + ':  ' + message)
         sock_client.sendto(message.encode('UTF-8'), (SERVER_ADDR, SERVER_PORT))
 
     sock_client.close()
 
 def main():
-    print('Welcome to PyChat! ')
+    print('Welcome to PyChat! \n')
     th_server, th_client = Thread(target = server_side), Thread(target = client_side)
     th_server.start(), th_client.start()
     th_server.join(),th_client.join()
