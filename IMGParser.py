5e3be6d1bf3e5c8bf9fedc54a03adb0bcdf6f55a
Aleksey <sidorenkoalekse@mail.ru>
2021-11-19 08:28:46 +0700

    Update and rename JpegParser.py to IMGParser.py

---
+++ IMGParser.py
@@ -0,0 +1,28 @@
+import requests, os, urllib.request, wget
+from bs4 import BeautifulSoup as BS
+
+count = 0
+i = 0
+
+while count != 82000:
+    path ="D:/Downloads/Archive/messages/2000000015/messages" + str(count) + ".html"
+    links = []
+
+    file = open(path, "r")
+    def parser(f):
+        for line in f:
+            if "=album" in line:
+                links.append(line[36:].split("'")[0])
+            else:
+                pass
+        file.close()
+        download(i)
+
+    def download(i):
+        for link in links:
+            wget.download(link, "M:/downloads_images/photo" + str(i) + ".jpg")
+            i += 1
+
+    parser(file)
+    count += 50
+    i += count
